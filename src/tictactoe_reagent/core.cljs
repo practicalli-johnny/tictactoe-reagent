b7212df55b119d97e75559d45c60feaaaffbff2a
John Stevenson <john@jr0cket.co.uk>
2017-10-22 16:13:24 +0100

    Designing the game board for tictactoe
    Experiments in the REPL on how to generate a game board, rather than simply
    hard-code the board

--- src/tictactoe_reagent/core.cljs
+++ src/tictactoe_reagent/core.cljs
@@ -11,6 +11,13 @@
 
 (defonce app-state (atom {:text "Lets Play TicTacToe"}))
 
+(defn game-board
+  "Create a data structure to represent the values of cells in the game board.
+  A vector is used to hold the overall game board
+  Each nested vector represents a line of the game board.
+  Dimension specifies the size of the game board."
+  [dimension]
+  (vec (repeat dimension (vec (repeat dimension :empty)))))
 
 
 (defn tictactoe-game []
@@ -26,3 +33,34 @@
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; REPL Experiments
+
+;; Generating a data structure to represet the game board
+
+;; We could just hard code the board as follows, although that limits us to a specific size of board:
+#_[[:empty :empty :empty]
+   [:empty :empty :empty]
+   [:empty :empty :empty]]
+
+;; To create a row is simple to do using the repeat function to generate 3 :empty keywords and return them as a list
+#_(repeat 3 :empty)
+;; => (:empty :empty :empty)
+
+;; To make this a vector we can just wrap that in a vec function
+#_(vec (repeat 3 :empty))
+;; => [:empty :empty :empty]
+
+;; To create three rows we just repeat the code above 3 times
+
+#_(vec (repeat 3 (vec (repeat 3 :empty))))
+;; => [[:empty :empty :empty] [:empty :empty :empty] [:empty :empty :empty]]
+
+;; we can use the above code in a function and replace 3 with a local name that takes the value of the argument passed in
+;; so lets write a game-board function.
+
+#_(println (game-board 3))
